e0dd95c9e1991d56a1cb12e13ca3f46dbf839a98
GummieHihihi <innerpeace1803@gmail.com>
2022-03-09 15:03:45 +0700

    dsa

--- Duong/src/main/java/MainPineLine.java
+++ Duong/src/main/java/MainPineLine.java
@@ -2,24 +2,55 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.schemas.Schema;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.windowing.AfterPane;
-import org.apache.beam.sdk.transforms.windowing.AfterWatermark;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
-import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PCollectionTuple;
-import org.apache.beam.sdk.values.TupleTag;
-import org.apache.beam.sdk.values.TupleTagList;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.windowing.*;
+import org.apache.beam.sdk.values.*;
 import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+
+/**
+ * The {@link StreamingMinuteTrafficPipeline} is a sample pipeline which can be used as a base for creating a real
+ * Dataflow pipeline.
+ *
+ * <p><b>Pipeline Requirements</b>
+ *
+ * <ul>
+ *   <li>Requirement #1
+ *   <li>Requirement #2
+ * </ul>
+ *
+ * <p><b>Example Usage</b>
+ *
+ * <pre>
+ * # Set the pipeline vars
+ * PROJECT_ID=PROJECT_ID
+ * PIPELINE_FOLDER=gs://${PROJECT_ID}/dataflow/pipelines/sample-pipeline
+ *
+ * # Set the runner
+ * RUNNER=DataflowRunner
+ *
+ * # Build the template
+ * mvn compile exec:java \
+ * -Dexec.mainClass=com.mypackage.pipeline.BatchUserTrafficPipeline \
+ * -Dexec.cleanupDaemonThreads=false \
+ * -Dexec.args=" \
+ * --project=${PROJECT_ID} \
+ * --stagingLocation=${PIPELINE_FOLDER}/staging \
+ * --tempLocation=${PIPELINE_FOLDER}/temp \
+ * --runner=${RUNNER} \
+ * ADDITIONAL PARAMETERS HERE"
+ * </pre>
+ */
 
 public class MainPineLine {
     static final TupleTag<com.mypackage.pipeline.Account> parsedMessages = new TupleTag<com.mypackage.pipeline.Account>() {
@@ -71,7 +102,7 @@ public class MainPineLine {
             @Override
             public PCollectionTuple expand(PCollection<String> input) {
                 return input
-                        .apply("JsonToCommonLog", ParDo.of(new DoFn<String, com.mypackage.pipeline.Account>() {
+                        .apply("Json to object account", ParDo.of(new DoFn<String, com.mypackage.pipeline.Account>() {
                                     @ProcessElement
                                     public void processElement(ProcessContext context) {
                                         String json = context.element();
@@ -105,6 +136,7 @@ public class MainPineLine {
         Options options = PipelineOptionsFactory.fromArgs(args)
                 .withValidation()
                 .as(Options.class);
+        System.out.println(options.getInputTopic() + options.getDeadletterBucket());
         run(options);
     }
 
@@ -129,54 +161,55 @@ public class MainPineLine {
                                 // Retrieve timestamp information from Pubsub Message attributes
                                 .withTimestampAttribute("timestamp")
                                 .fromTopic(options.getInputTopic()))
-                        .apply("ConvertMessageToCommonLog", new PubsubMessageToCommonLog());
+                        .apply("convert Message to Account", new PubsubMessageToAccount());
 
         // Write parsed messages to BigQuery
-        transformOut
+        String testing = transformOut
                 // Retrieve parsed messages
                 .get(parsedMessages)
-                .apply("WindowByMinute", Window.<com.mypackage.pipeline.Account>into(
-                                FixedWindows.of(Duration.standardSeconds(options.getWindowDuration()))).withAllowedLateness(
-                                Duration.standardDays(options.getAllowedLateness()))
-                        .triggering(AfterWatermark.pastEndOfWindow()
-                                .withLateFirings(AfterPane.elementCountAtLeast(1)))
-                        .accumulatingFiredPanes())
-                // update to Group.globally() after resolved: https://issues.apache.org/jira/browse/BEAM-10297
-                // Only if supports Row output
-                .apply("CountPerMinute", Combine.globally(Count.<CommonLog>combineFn())
-                        .withoutDefaults())
-                .apply("ConvertToRow", ParDo.of(new DoFn<Long, Row>() {
-                    @ProcessElement
-                    public void processElement(@Element Long views, OutputReceiver<Row> r, IntervalWindow window) {
-                        Instant i = Instant.ofEpochMilli(window.end()
-                                .getMillis());
-                        Row row = Row.withSchema(pageviewsSchema)
-                                .addValues(views, i)
-                                .build();
-                        r.output(row);
-                    }
-                }))
-                .setRowSchema(pageviewsSchema)
-                // TODO: is this a streaming insert?
-                .apply("WriteToBQ", BigQueryIO.<Row>write().to(options.getOutputTableName())
-                        .useBeamSchema()
-                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
-                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+                .toString();
+        System.out.println(testing);
+//                .apply("WindowByMinute", Window.<com.mypackage.pipeline.Account>into(
+//                                FixedWindows.of(Duration.standardSeconds(options.getWindowDuration()))).withAllowedLateness(
+//                                Duration.standardDays(options.getAllowedLateness()))
+//                        .triggering(AfterWatermark.pastEndOfWindow()
+//                                .withLateFirings(AfterPane.elementCountAtLeast(1)))
+//                        .accumulatingFiredPanes())
+//                // update to Group.globally() after resolved: https://issues.apache.org/jira/browse/BEAM-10297
+//                // Only if supports Row output
+//                .apply("CountPerMinute", Combine.globally(Count.<com.mypackage.pipeline.Account>combineFn())
+//                        .withoutDefaults())
+//                .apply("ConvertToRow", ParDo.of(new DoFn<Long, Row>() {
+//                    @ProcessElement
+//                    public void processElement(@Element Long views, OutputReceiver<Row> r, IntervalWindow window) {
+//                        Instant i = Instant.ofEpochMilli(window.end()
+//                                .getMillis());
+//                        Row row = Row.withSchema(pageviewsSchema)
+//                                .addValues(views, i)
+//                                .build();
+//                        r.output(row);
+//                    }
+//                }))
+//                .setRowSchema(pageviewsSchema)
+//                 TODO: is this a streaming insert?
+//                .apply("WriteToBQ", BigQueryIO.<Row>write().to(options.getOutputTableName())
+//                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+//                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
 
         // Write unparsed messages to Cloud Storage
-        transformOut
-                // Retrieve unparsed messages
-                .get(unparsedMessages)
-                .apply("FireEvery10s", Window.<String>configure().triggering(
-                                Repeatedly.forever(
-                                        AfterProcessingTime.pastFirstElementInPane()
-                                                .plusDelayOf(Duration.standardSeconds(10))))
-                        .discardingFiredPanes())
-                .apply("WriteDeadletterStorage", TextIO.write()
-                        //TODO: change this to actual full parameter
-                        .to(options.getDeadletterBucket() + "/deadletter/*")
-                        .withWindowedWrites()
-                        .withNumShards(10));
+//        transformOut
+//                // Retrieve unparsed messages
+//                .get(unparsedMessages)
+//                .apply("FireEvery10s", Window.<String>configure().triggering(
+//                                Repeatedly.forever(
+//                                        AfterProcessingTime.pastFirstElementInPane()
+//                                                .plusDelayOf(Duration.standardSeconds(10))))
+//                        .discardingFiredPanes())
+//                .apply("WriteDeadletterStorage", TextIO.write()
+//                        TODO: change this to actual full parameter
+//                        .to(options.getDeadletterBucket() + "/deadletter/*")
+//                        .withWindowedWrites()
+//                        .withNumShards(10));
 
 
         return pipeline.run();
