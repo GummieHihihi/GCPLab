ea77b9da7f7c9b8254c3ac78fb1e2df77152733f
GummieHihihi <innerpeace1803@gmail.com>
2022-04-04 00:24:29 +0700

    Update Send_Data.java

--- Duong/src/main/java/Send_Data.java
+++ Duong/src/main/java/Send_Data.java
@@ -34,7 +34,7 @@ public class Send_Data {
 
         GenerateData dataFactory = new GenerateData();
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 500; i++) {
             System.out.printf("Publish message %dth in Topic \n", i);
 //            #generate right wrong topic
             Random random = new Random();
